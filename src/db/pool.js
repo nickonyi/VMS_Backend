349c10588379faed6a4205af315b79a2557127e9
nickonyi <nickoluoch1130@gmail.com>
2026-08-18 12:42:38 +0300

    fix: update database connection string to use DATABASE_URL

--- src/db/pool.js
+++ src/db/pool.js
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 import "dotenv/config";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL_PROD,
+  connectionString: process.env.DATABASE_URL,
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
